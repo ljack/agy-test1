bda4fc706201131974d990616d4499922faa73d8
Jarkko Lietolahti <Jarkko.lietolahti@gmail.com>
2026-05-28 09:41:19 +0300

    test: add initial test targets

---
+++ Tests/agy-test1Tests/agy_test1Tests.swift
@@ -0,0 +1,8 @@
+import Testing
+@testable import agy_test1
+
+@Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    // Swift Testing Documentation
+    // https://developer.apple.com/documentation/testing
+}
